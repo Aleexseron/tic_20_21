da0cdea571b82261ee05985f5491173ea2ebf0fc
Aleexseron <71437819+Aleexseron@users.noreply.github.com>
2020-10-29 12:29:01 +0100

    Add files via upload

---
+++ semanita.py
@@ -0,0 +1,19 @@
+def semanita():
+     dia=input("Introduce un numero del 1 al 7:")
+    if dia==1:
+        print "LUNES"
+    if dia==2:
+        print "MARTES"
+    if dia==3:
+        print "MIERCOLES"
+    if dia==4:
+        print "JUEVES"
+    if dia==5:
+        print "CHAVALEEEESSS QUE ES VIERNEEEEES TUTURUUU TUTURUUU"
+    if dia==6:
+        print "SABADITO"
+    if dia==7:
+        print "DOMINGUITO"
+
+
+semanita()
